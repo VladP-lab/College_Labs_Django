c4198e0b0591eeed4f369e5ef3927db87747e2c8
Vlad P <wilamkopa393@gmail.com>
2026-03-05 21:23:58 +0200

    Lab 4: Created models for Python Guide and configured Admin panel

--- labs_app/admin.py
+++ labs_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Topic, PythonLibrary
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')
+
+@admin.register(PythonLibrary)
+class PythonLibraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
--- labs_app/models.py
+++ labs_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    # Зв'язок: одна категорія має багато тем
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категорія")
+    title = models.CharField(max_length=200, verbose_name="Назва теми")
+    description = models.TextField(verbose_name="Опис теми")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.title
+
+class PythonLibrary(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва бібліотеки")
+    usage = models.CharField(max_length=255, verbose_name="Для чого потрібна")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
+
+    def __str__(self):
+        return self.name
